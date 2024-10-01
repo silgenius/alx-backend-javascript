a678acbea5800096165907884897864e96d679b8
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-10-01 01:48:34 +0100

    Update 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -5,11 +5,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
+    getIncomeInDollars (income) {
       return `$${income}`;
     },
 
-    getIncomeInEuros(income) {
+    getIncomeInEuros (income) {
       return `${income} euros`;
     },
   };
