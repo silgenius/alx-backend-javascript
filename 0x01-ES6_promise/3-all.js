c3aaa75d84492e9e78251d639fdf00d0d83302af
silgenius <olutademartin@gmail.com>
2024-10-04 22:54:22 +0000

    Fix requirement error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()])
+  return Promise.all([createUser(), uploadPhoto()])
     .then(([user, photo]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      return console.log("Signup system offline");
+      console.log('Signup system offline');
     });
 }
