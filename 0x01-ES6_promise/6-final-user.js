cc623bd900e02edec3e632a3ad726eeba444b4f7
silgenius <olutademartin@gmail.com>
2024-10-10 10:24:33 +0000

    Implement solution to task

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const newArray = [];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      results.forEach((result) => {
+        newArray.push({ status: result.status, value: result.value || result.reason });
+      });
+      return newArray;
+    });
+}
