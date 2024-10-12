a5e6e5903c7f60e0516db1d619d097b727c4a81a
silgenius <olutademartin@gmail.com>
2024-10-13 00:53:24 +0100

    Implement solution to task

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    } else {
+      this._sqft = sqft;
+    }
+
+    if (this.constructor !== Building) {
+      this.evacuationWarningMessage();
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
