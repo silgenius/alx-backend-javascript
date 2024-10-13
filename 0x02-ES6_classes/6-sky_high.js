e19b704c531dcc4adebc79460803a3bafa406486
silgenius <olutademartin@gmail.com>
2024-10-13 17:03:49 +0100

    Implement solution to task

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building'
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+        throw new TypeError('floors must be a number');
+    } else {
+        this._floors = floors;
+    }
+  }
+
+  static evacuationWarningMessage() {
+    return "Evacuate slowly the NUMBER_OF_FLOORS floors";
+  }
+
+  evacuationWarningMessage () {
+    return "Evacuate slowly the NUMBER_OF_FLOORS floors";
+  }
+
+  get floors() {
+    return this._floors;
+  }
+}
