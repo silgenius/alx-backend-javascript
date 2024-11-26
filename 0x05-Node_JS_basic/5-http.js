a423c5f14850376dbddbac5968d0c1348ea7c6b0
silgenius <olutademartin@gmail.com>
2024-11-26 12:28:09 +0100

    Change env

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http');
+const { PassThrough } = require('stream');
+const url  = require('url')
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = '1245';
+
+const app = createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+//   res.setHeader('Content-Type', 'text/plain');
+  if (parsedUrl.pathname === '/') {
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
+    res.write('This is the list of our students')
+    countStudents(process.argv[2]).then(() => {}
+    )
+    res.end()
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+})
