2c74c2a197d83cdbd22369e831ee9b3d82cd7138
silgenius <olutademartin@gmail.com>
2024-11-28 15:29:46 +0100

    Implement solution to task 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,18 @@
+const { describe, it } = require('mocha');
+const chai = require('chai');
+const request = require('request');
+const { expect } = chai;
+
+const url = 'http://localhost:7865/'
+describe("test express web app", function() {
+    it("tests that the web app returns the Correct Status code", function(done) {
+        request(url, (error, response, body) => {
+            if (error) {
+                done(error);
+            }
+            expect(response.statusCode).to.equal(200)
+            expect(body).to.equal('Welcome to the payment system');
+        });
+        done();
+    })
+})
