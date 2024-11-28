24d05ad441f48e6b35c1891e22cfee83d3694b79
silgenius <olutademartin@gmail.com>
2024-11-28 15:50:33 +0100

    Fix error in code

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,14 +5,14 @@ const { expect } = chai;
 
 const url = 'http://localhost:7865/'
 describe("test express web app", function() {
-    it("tests that the web app returns the Correct Status code", function(done) {
+    it("tests that the web app returns the Correct Status code and resp", function(done) {
         request(url, (error, response, body) => {
             if (error) {
                 done(error);
             }
             expect(response.statusCode).to.equal(200)
             expect(body).to.equal('Welcome to the payment system');
+            done();
         });
-        done();
     })
 })
